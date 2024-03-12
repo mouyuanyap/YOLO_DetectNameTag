9702545b070201e1ddc200a9ef310ccc5862a0f6
Mou Yuan Yap <mouyuanyap@yahoo.com>
2024-03-12 20:52:33 +0800

    Add training custom nametag dataset

---
+++ train_tag_yolo/train.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+
+# Train the model with 2 GPUs
+results = model.train(data='/Users/mouyuanyap/Documents/vscode/train_tag_yolo/dataset.yaml', epochs=50, imgsz=640,batch=8)
+
