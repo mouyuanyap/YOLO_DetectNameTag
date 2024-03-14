c37db2957e9f386bacc8169ddcc12690b1afb612
YMY <mouyuanyap@yahoo.com>
2024-03-14 22:16:01 +0800

    update test.py

--- train_tag_yolo/test.py
+++ train_tag_yolo/test.py
@@ -1,8 +1,8 @@
 import ultralytics, cv2
 from pprint import pprint
-model = ultralytics.YOLO('/Users/mouyuanyap/Documents/vscode/train_tag_yolo/runs/detect/train4/weights/best.pt')
+model = ultralytics.YOLO('./train_tag_yolo/runs/detect/train4/weights/best.pt')
 
-image = "/Users/mouyuanyap/Documents/vscode/footfallCam_Task/runs/detect/test2/crops/person/labels868.jpg"
-results = model.predict(source = image, save=True, save_txt=True, name = "test", exist_ok=True, save_crop = True)
+image = "./runs/detect/test2/crops/person/labels868.jpg"
+results = model.predict(source = image, save=True, save_txt=True, name = "tag_test", exist_ok=True, save_crop = True)
 
 print(results)
